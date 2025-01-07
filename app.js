35999fd6f83da515003b1971fac8f0eea8ede89b
qweee112 <rapha@Note>
2025-01-07 20:40:17 -0300

    selection sort

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import { bubbleSort } from "./sorting.js";
+import { bubbleSort, selectionSort } from "./sorting.js";
 
 const listItems = document.querySelectorAll(".algorithm");
+const bars = document.querySelectorAll(".bar");
 
 listItems.forEach((li) => {
   li.addEventListener("click", function () {
@@ -14,3 +15,35 @@ listItems.forEach((li) => {
 const getRandom = function () {
   return Math.trunc(Math.random() * 100 + 1);
 };
+
+console.log(document.querySelector(".arraySize"));
+
+function generateArray(value) {
+  let globalArray = [];
+  while (globalArray.length !== value) {
+    globalArray.push(Math.trunc(Math.random() * 100 + 1));
+  }
+  adjustBars(globalArray);
+  return globalArray;
+}
+
+function adjustBars(array) {
+  const barsLength = bars.length;
+  const arrayLength = array.length;
+  for (let i = 0; i < arrayLength; i++) {
+    if (barsLength !== arrayLength) {
+      for (
+        let j = 0;
+        j <
+        (arrayLength > barsLength
+          ? arrayLength - barsLength
+          : barsLength - arrayLength);
+        j++
+      ) {
+        document.querySelector(".barContainer").createDiv(".bar");
+      }
+    }
+    document.querySelectorAll(".bar")[i].style.height = array[i];
+    document.querySelectorAll(".bar")[i].innerText = array[i] + "%";
+  }
+}
--- sorting.js
+++ sorting.js
@@ -13,3 +13,23 @@ export function bubbleSort(array) {
   }
   return array;
 }
+
+export function selectionSort(array) {
+  let index;
+  let smallest;
+  for (let i = 0; i < array.length - 1; i++) {
+    index = i;
+    smallest = array[i];
+    for (let j = i; j < array.length - 1; j++) {
+      if (smallest < array[j + 1]) {
+        continue;
+      } else {
+        index = j + 1;
+        smallest = array[j + 1];
+      }
+    }
+    array[index] = array[i];
+    array[i] = smallest;
+  }
+  return array;
+}
