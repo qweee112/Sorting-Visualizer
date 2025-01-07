f46ddcc9bb25330b6ecb2327b04ef7a37097aec9
qweee112 <rapha@Note>
2025-01-07 15:05:36 -0300

    bubble sort

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+import { bubbleSort } from "./sorting.js";
 
 const listItems = document.querySelectorAll(".algorithm");
 
-listItems.forEach(li => {
-  li.addEventListener('click', function () {
-    listItems.forEach(li => {
-      li.style.backgroundColor = "black";
-    })
-    li.style.backgroundColor = "#005B41"
-  })
-})
+listItems.forEach((li) => {
+  li.addEventListener("click", function () {
+    listItems.forEach((li) => {
+      li.style.backgroundColor = "var(--darkBlue)";
+    });
+    li.style.backgroundColor = "var(--blue)";
+  });
+});
 
-const getRandom = function () {return Math.trunc(Math.random() * 100 + 1)}
-
-
+const getRandom = function () {
+  return Math.trunc(Math.random() * 100 + 1);
+};
---
+++ sorting.js
@@ -0,0 +1,15 @@
+export function bubbleSort(array) {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        swapped = true;
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
